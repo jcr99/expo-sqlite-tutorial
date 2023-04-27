3b66b3aef3d5d8cd30e0dcdf5e457ddff8e11ed8
John Rodriguez <john.rodriguez@monday-consulting.com>
2023-04-27 08:25:44 +0200

    xyx

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Platform } from 'react-native';
-import * as SQLite from 'expo-sqlite';
-import { useState, useEffect } from 'react';
-import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
-import * as DocumentPicker from 'expo-document-picker';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Platform,
+} from "react-native";
+import * as SQLite from "expo-sqlite";
+import { useState, useEffect } from "react";
+import * as Sharing from "expo-sharing";
+import * as FileSystem from "expo-file-system";
+import * as DocumentPicker from "expo-document-picker";
 
 // expo add expo-sqlite
 // expo add expo-file-system
@@ -39,69 +46,88 @@ import * as DocumentPicker from 'expo-document-picker';
   expo start --dev-client
 */
 
-
 export default function App() {
-  const [db, setDb] = useState(SQLite.openDatabase('example.db'));
+  const [db, setDb] = useState(SQLite.openDatabase("example.db"));
   const [isLoading, setIsLoading] = useState(true);
   const [names, setNames] = useState([]);
   const [currentName, setCurrentName] = useState(undefined);
 
   const exportDb = async () => {
     if (Platform.OS === "android") {
-      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      const permissions =
+        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
       if (permissions.granted) {
         const base64 = await FileSystem.readAsStringAsync(
-          FileSystem.documentDirectory + 'SQLite/example.db',
+          FileSystem.documentDirectory + "SQLite/example.db",
           {
-            encoding: FileSystem.EncodingType.Base64
+            encoding: FileSystem.EncodingType.Base64,
           }
         );
 
-        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'example.db', 'application/octet-stream')
-        .then(async (uri) => {
-          await FileSystem.writeAsStringAsync(uri, base64, { encoding : FileSystem.EncodingType.Base64 });
-        })
-        .catch((e) => console.log(e));
+        await FileSystem.StorageAccessFramework.createFileAsync(
+          permissions.directoryUri,
+          "example.db",
+          "application/octet-stream"
+        )
+          .then(async (uri) => {
+            await FileSystem.writeAsStringAsync(uri, base64, {
+              encoding: FileSystem.EncodingType.Base64,
+            });
+          })
+          .catch((e) => console.log(e));
       } else {
         console.log("Permission not granted");
       }
     } else {
-      await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/example.db');
+      await Sharing.shareAsync(
+        FileSystem.documentDirectory + "SQLite/example.db"
+      );
     }
-  }
+  };
 
   const importDb = async () => {
     let result = await DocumentPicker.getDocumentAsync({
-      copyToCacheDirectory: true
+      copyToCacheDirectory: true,
     });
 
-    if (result.type === 'success') {
+    if (result.type === "success") {
       setIsLoading(true);
-      
-      if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
-        await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+
+      if (
+        !(
+          await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite")
+        ).exists
+      ) {
+        await FileSystem.makeDirectoryAsync(
+          FileSystem.documentDirectory + "SQLite"
+        );
       }
 
-      const base64 = await FileSystem.readAsStringAsync(
-        result.uri,
-        {
-          encoding: FileSystem.EncodingType.Base64
-        }
-      );
+      const base64 = await FileSystem.readAsStringAsync(result.uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
 
-      await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'SQLite/example.db', base64, { encoding: FileSystem.EncodingType.Base64 });
+      await FileSystem.writeAsStringAsync(
+        FileSystem.documentDirectory + "SQLite/example.db",
+        base64,
+        { encoding: FileSystem.EncodingType.Base64 }
+      );
       await db.closeAsync();
-      setDb(SQLite.openDatabase('example.db'));
+      setDb(SQLite.openDatabase("example.db"));
     }
   };
 
   useEffect(() => {
-    db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
+      );
     });
 
-    db.transaction(tx => {
-      tx.executeSql('SELECT * FROM names', null,
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM names",
+        null,
         (txObj, resultSet) => setNames(resultSet.rows._array),
         (txObj, error) => console.log(error)
       );
@@ -119,25 +145,29 @@ export default function App() {
   }
 
   const addName = () => {
-    db.transaction(tx => {
-      tx.executeSql('INSERT INTO names (name) values (?)', [currentName],
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO names (name) values (?)",
+        [currentName],
         (txObj, resultSet) => {
           let existingNames = [...names];
-          existingNames.push({ id: resultSet.insertId, name: currentName});
+          existingNames.push({ id: resultSet.insertId, name: currentName });
           setNames(existingNames);
           setCurrentName(undefined);
         },
         (txObj, error) => console.log(error)
       );
     });
-  }
+  };
 
   const deleteName = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('DELETE FROM names WHERE id = ?', [id],
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM names WHERE id = ?",
+        [id],
         (txObj, resultSet) => {
           if (resultSet.rowsAffected > 0) {
-            let existingNames = [...names].filter(name => name.id !== id);
+            let existingNames = [...names].filter((name) => name.id !== id);
             setNames(existingNames);
           }
         },
@@ -147,12 +177,16 @@ export default function App() {
   };
 
   const updateName = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('UPDATE names SET name = ? WHERE id = ?', [currentName, id],
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE names SET name = ? WHERE id = ?",
+        [currentName, id],
         (txObj, resultSet) => {
           if (resultSet.rowsAffected > 0) {
             let existingNames = [...names];
-            const indexToUpdate = existingNames.findIndex(name => name.id === id);
+            const indexToUpdate = existingNames.findIndex(
+              (name) => name.id === id
+            );
             existingNames[indexToUpdate].name = currentName;
             setNames(existingNames);
             setCurrentName(undefined);
@@ -168,8 +202,8 @@ export default function App() {
       return (
         <View key={index} style={styles.row}>
           <Text>{name.name}</Text>
-          <Button title='Delete' onPress={() => deleteName(name.id)} />
-          <Button title='Update' onPress={() => updateName(name.id)} />
+          <Button title="Delete" onPress={() => deleteName(name.id)} />
+          <Button title="Update" onPress={() => updateName(name.id)} />
         </View>
       );
     });
@@ -177,7 +211,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput value={currentName} placeholder='name' onChangeText={setCurrentName} />
+      <TextInput
+        value={currentName}
+        placeholder="name"
+        onChangeText={setCurrentName}
+      />
       <Button title="Add Name" onPress={addName} />
       {showNames()}
       <Button title="Export Db" onPress={exportDb} />
@@ -190,15 +228,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    justifyContent: 'space-between',
-    margin: 8
-  }
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "stretch",
+    justifyContent: "space-between",
+    margin: 8,
+  },
 });
